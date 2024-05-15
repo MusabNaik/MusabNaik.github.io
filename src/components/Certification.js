c6ea170fa327bee0f178bc1eb2573e820bfce013
musabnaik <musab944@gmail.com>
2024-05-15 19:28:50 -0400

    Google Data Analytics Cert added

--- src/components/Certification.js
+++ src/components/Certification.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import pl300 from './images/PL-300.png'
 import ai900 from './images/AI-900.png'
+import GDA from './images/GDA.png'
 
 const Certification = () => {
   return (
     <div className="container mt-5">
       <h2>Certifications</h2>
       <ul className="list-group mt-3">
-        <a href="https://learn.microsoft.com/api/credentials/share/en-us/MusabNaik/2ED09E3FEB316342?sharingId=4E3BF16DC2BBFC3F">
+        <a href="https://learn.microsoft.com/api/credentials/share/en-us/MusabNaik/2ED09E3FEB316342?sharingId=4E3BF16DC2BBFC3F" target="_blank" rel="noopener noreferrer">
         <li className="list-group-item"><b> Microsoft Certified: Power BI Data Analyst Associate </b>
         <br></br>
         <br></br>
@@ -19,7 +20,7 @@ const Certification = () => {
         </li>
         <br></br>
         </a>
-        <a href="https://learn.microsoft.com/api/credentials/share/en-us/MusabNaik/2EFBFE1D4FD868D0?sharingId=4E3BF16DC2BBFC3F">
+        <a href="https://learn.microsoft.com/api/credentials/share/en-us/MusabNaik/2EFBFE1D4FD868D0?sharingId=4E3BF16DC2BBFC3F" target="_blank" rel="noopener noreferrer">
         <li className="list-group-item"><b> Microsoft Certified: Azure AI Fundamentals </b>
         <br></br>
         <br></br>
@@ -29,8 +30,19 @@ const Certification = () => {
           style={{ width: '44em', height: 'auto' }}
           />
         </li>
+        <br></br>
         </a>
-        <li className="list-group-item">Google Data Analytics Professional Certificate</li>
+        <a href="https://www.coursera.org/account/accomplishments/professional-cert/H82NZ45H8QQ2" target="_blank" rel="noopener noreferrer">
+        <li className="list-group-item"><b> Google Data Analytics Professional Certificate </b>
+        <br></br>
+        <br></br>
+        <img
+          src={GDA}
+          alt="Example"
+          style={{ width: '44em', height: 'auto' }}
+          />
+        </li>
+        </a>
       </ul>
     </div>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,8 +8,9 @@ const Contact = () => {
         You can reach out to me via email or connect with me on LinkedIn:
       </p>
       <ul className="list-group mt-3">
-        <li className="list-group-item">Email: <a href="mailto:dataanalyst@example.com">dataanalyst@example.com</a></li>
-        <li className="list-group-item">LinkedIn: <a href="https://www.linkedin.com/in/your-profile">linkedin.com/in/your-profile</a></li>
+        <li className="list-group-item">Email: <a href="mailto:musab944@gmail.com" target="_blank" rel="noopener noreferrer">musab944@gmail.com</a></li>
+        <li className="list-group-item">LinkedIn: <a href="https://www.linkedin.com/in/Musab-Naik" target="_blank" rel="noopener noreferrer">linkedin.com/in/Musab-Naik</a></li>
+        <li className="list-group-item">GitHub: <a href="https://github.com/MusabNaik" target="_blank" rel="noopener noreferrer">https://github.com/MusabNaik</a></li>
       </ul>
     </div>
   );
